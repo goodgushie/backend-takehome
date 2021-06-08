890419ab3fd161e89f3c1ca8e880ca7b0f31b714
Rocky Gettel <rgettel@nethealth.com>
2021-06-07 23:10:26 -0400

    Remove unneeded using statements, added StateFactor test

---
+++ src/Quotes/Quotes.Test/StateFactorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quotes.Domain;
+using System.Collections.Generic;
+
+namespace Quotes.Test
+{
+    [TestClass]
+    public class StateFactorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void FromAbbreviation_UnknownState_ThrowsException()
+        {
+            // Arrange
+            // Act
+            // Assert
+            StateFactor.FromAbbreviation("ZZ");
+        }
+    }
+}
